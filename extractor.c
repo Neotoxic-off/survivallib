71f41e9e725ddbc8bc84dd872b8afc2b43495171
N Ξ Θ <paul.gardien@epitech.eu>
2020-01-17 13:35:50 +0100

    Update extractor.c

--- extractor.c
+++ extractor.c
@@ -1,26 +1,19 @@
-/*
-** USAGE:
-**      extractor("STRING WHERE YOU WANT TO EXTRACT", <place of the word to extract>)
-** EXAMPLE:
-**      printf("String extracted: %s\n", extractor("String test for extractor", 3));
-**      String extracted:for
-*/
-
-char *extractor(char *string, int place)
+char *extractor(char *string, int place, char character)
 {
     int space = 1;
     char *output = malloc(sizeof(char) * strlen(string) + 1);
 
     for (int i = 0; string[i] != '\0'; i++) {
         for (; space < place; i++) {
-            if (string[i] == ' ' || string[i] == '\0')
+            if (string[i] == character || string[i] == '\0')
                 space++;
         }
         for (int index = 0; space == place; i++, index++) {
             output[index] = string[i];
-            if (string[i + 1] == ' ' || string[i + 1] == '\0')
+            if (string[i + 1] == character || string[i + 1] == '\0')
                 space++;
         }
     }
+    output[strlen(string)] = '\0';
     return (output);
 }

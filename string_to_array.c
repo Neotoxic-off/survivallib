e332f186feb3ed78de6751ea37729befe1a1efa2
N Ξ Θ <paul.gardien@epitech.eu>
2020-01-22 15:46:18 +0100

    Update string_to_array.c

--- string_to_array.c
+++ string_to_array.c
@@ -1,13 +1,13 @@
-void string_to_array(char ***array, char *string, char separator)
+void string_to_array(char **array, char *string, char separator)
 {
     int i = 0;
     int characters = count_character(string, separator);
     *array = malloc(sizeof(char *) * (characters + 2));
 
     for (; i <= characters; i++) {
-        (*array)[i] = my_malloc(sizeof(char) *
+        array[i] = my_malloc(sizeof(char) *
         (strlen(extractor(string, i, separator)) + 1));
-        (*array)[i] = extractor(string, (i + 1), separator);
+        array[i] = extractor(string, (i + 1), separator);
     }
-    (*array)[characters + 1] = NULL;
+    array[characters + 1] = NULL;
 }
